3f81c8eff09a17258ba8b3e3433518c879d6a071
vadim <vadvsemurad@gmail.com>
2023-04-04 15:11:16 +0200

    switch on self branch with suppressed warnings

--- third_party/libev.cmake
+++ third_party/libev.cmake
@@ -2,7 +2,7 @@ cmake_minimum_required(VERSION 3.14.0)
 include(FetchContent)
 FetchContent_Declare(
   libev
-  GIT_REPOSITORY https://github.com/mksdev/libev-cmake.git
+  GIT_REPOSITORY https://github.com/vdm4k/libev-cmake
 )
 
 FetchContent_MakeAvailable(libev)
